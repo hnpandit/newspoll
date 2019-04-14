5abe30f59a2b14f4becfbce4c01f30c3f293df84
Himanshu Pandit <himanshu.pandit@outlook.com>
2019-04-13 21:02:50 -0400

    changing mongo URI for heroku hosting

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
